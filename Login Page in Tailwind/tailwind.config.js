23d0124089f6af495008a7852cb8695d80295d00
Arjav Patel <arjav1528@gmail.com>
2024-12-22 22:35:17 +0530

    Login Page banaya hai using Tailwind

---
+++ Login Page in Tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        'custom' : 0xFF111029
+      },
+    },
+  },
+  plugins: [],
+}
+
