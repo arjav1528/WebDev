5c90e64985785949548b9db4cad42c2ff8a90d90
Arjav Patel <arjav1528@gmail.com>
2024-12-27 09:44:45 +0530

    DOM-2

--- DOM & BOM/script.js
+++ DOM & BOM/script.js
@@ -7,3 +7,26 @@ document
         console.log(paragraph.innerHTML)
         paragraph.innerHTML = "Jai hind Dosto"; 
     });
+
+
+
+// document
+//     .getElementById("highlight")
+//     .addEventListener("click", function() {
+//         let hold = document.getElementById("citiesList");
+//         // console.log(hold.innerHTML)
+//         let cities = hold.firstElementChild;
+//         console.log(cities.classList.add("highlight"));
+        
+//     });
+
+document
+    .getElementById('highlight')
+    .addEventListener('click', function() {
+    let hold = document.getElementById('citiesList');
+    let cities = hold.children;
+    cities[0].classList.add('animation');
+    
+
+    
+});
