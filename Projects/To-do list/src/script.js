b3221975c58ddbaac35c75a824854c77412c9ad7
Arjav Patel <arjav1528@gmail.com>
2024-12-29 23:35:36 +0530

    Final Commit

--- Projects/To-do list/src/script.js
+++ Projects/To-do list/src/script.js
@@ -6,10 +6,10 @@ document.addEventListener('DOMContentLoaded',() => {
     let allTasks = JSON.parse(localStorage.getItem('task')) || [];
 
     allTasks.forEach((val) => {
+        console.log(val);
         renderTask(val);
         
-    }      
-    );
+    });
 
 
     submitButton.addEventListener('click', function(event) {
@@ -36,9 +36,17 @@ document.addEventListener('DOMContentLoaded',() => {
 
     });
 
-    function taskButton(taskName){
-        let style = "<div class=\"flex justify-between m-3\"><h1 class=\" text-white p-2 m-4 ml-10 rounded-[4px]\">" + taskName +"</h1><button class=\"text-white  mt-4 ml-4 mb-4 mr-10 p-1 w-36 hover:bg-purple-400 hover:transition-all hover:duration-200 hover:text-white\">Delete</button></div>";
-        return style;
+    function taskButton(task){
+        if(task.isDone){
+            let style = "<div class=\"flex justify-between m-3\"><h1 id=\"" + task.id + "\" class=\" text-white p-2 m-4 ml-10 rounded-[4px] line-through\">" + task.text +"</h1><button class=\"text-white  mt-4 ml-4 mb-4 mr-10 p-1 w-36 hover:bg-purple-400 hover:transition-all hover:duration-200 hover:text-white\">Delete</button></div>";
+            return style;
+        }
+        else{
+            let style = "<div class=\"flex justify-between m-3\"><h1 id=\"" + task.id + "\" class=\" text-white p-2 m-4 ml-10 rounded-[4px]\">" + task.text +"</h1><button class=\"text-white  mt-4 ml-4 mb-4 mr-10 p-1 w-36 hover:bg-purple-400 hover:transition-all hover:duration-200 hover:text-white\">Delete</button></div>";
+            return style;
+        
+        }
+        
     }
 
 
@@ -46,7 +54,7 @@ document.addEventListener('DOMContentLoaded',() => {
         
         const li = document.createElement("li");
         li.setAttribute("data-id",task.id);
-        li.innerHTML = taskButton(task.text);
+        li.innerHTML = taskButton(task);
         li.classList.add("flex");
         
         taskList.appendChild(li);
@@ -54,14 +62,17 @@ document.addEventListener('DOMContentLoaded',() => {
             if(e.target.innerHTML === "Delete"){
                 console.log(task);
                 taskList.removeChild(li);
+                saveTask();
             }
             else{
                 task.isDone = !task.isDone;
-                li.classList.toggle('line-through');
+                document.getElementById(task.id).classList.toggle("line-through");
+                saveTask();
+                
             }
             
         });
-        saveTask();
+        
     }
 
 
