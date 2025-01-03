b63f1eac645338e7df41644d614145862d885216
Arjav Patel <arjav1528@gmail.com>
2025-01-03 20:06:44 +0530

    Firse some random shit

---
+++ Projects/some random shit/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("submit").addEventListener("click", function(){
+        console.log(document.getElementById("email-input").value);
+        
+    });
+});
