f3aa0e4a11a0e77d73468fbcc2d86fb8283d3ef9
Arjav Patel <arjav1528@gmail.com>
2025-01-14 13:49:17 +0530

    Updated the Logic of the update task

--- Backend/2_node_project/index.js
+++ Backend/2_node_project/index.js
@@ -3,10 +3,12 @@ import express from 'express'
 const app = express();
 app.use(express.json());
 
-const port = 8000;
+const port = 4000;
 let tasks = [];
 let nextID = 1;
 
+
+// Create a new task
 app.post('/post', (req, res) => {
     const {name,due} = req.body;
     const newTask = {
@@ -20,10 +22,45 @@ app.post('/post', (req, res) => {
 });
 
 
+// Get all tasks
 
 app.get('/get', (req, res) => {
 
     res.send(tasks);
+});
+
+
+// Get a Task by ID
+app.get('/get/:id', async (req, res) => {
+    try {
+        const task = tasks.find(t => t.id === parseInt(req.params.id))
+        if (!task) {
+            return res.status(404).send('Task not found')
+        }
+        // res.json(task)
+        res.send(task);
+    } catch (error) {
+        res.status(404).send(error.message)
+    }
+})
+
+
+
+app.put('/update/:id', async (req, res) => {
+    try {
+        const { name, due } = req.body;
+        const task = tasks.find(t => t.id === parseInt(req.params.id));
+        if(!task){
+            res.send('No Task Found');
+        }
+        else{
+            task.name = name;
+            task.due = due;
+            res.send(tasks);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(port, () => {
