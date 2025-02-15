cd41f3356384fe3dbec4a1dde3992fb4e30e47f6
Arjav Patel <arjav1528@gmail.com>
2025-02-15 05:56:42 +0530

    Add new React app structure and remove unused files

---
+++ React/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <>
+      <h1>My React App</h1>
+      <p>Welcome to my React app!</p>
+      <Youtube />
+      
+    </>
+
+  );
+}
+
+export default App;
